bc81fcab81b7677f62a98b86df05ef6addede870
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2023-11-11 23:50:38 -0400

    firt commit

---
+++ renderer.js
@@ -0,0 +1,24 @@
+/**
+ * This file is loaded via the <script> tag in the index.html file and will
+ * be executed in the renderer process for that window. No Node.js APIs are
+ * available in this process because `nodeIntegration` is turned off and
+ * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
+ * to expose Node.js functionality from the main process.
+ */
+//console.log('Hello world!'); 
+
+//C:\Users\Diseño\Downloads\Documents\markdown-editor\marked\
+const marked = require('./marked/bin');
+const markdownView = document.querySelector('#markdown');
+const htmlView = document.querySelector('#html'); 
+
+const renderToMarkdown = (markdown) => {
+    //htmlView.innerHTML = 'asfaljsfjasjdf';
+    htmlView.innerHTML = marked(markdown, {sanitize: true});
+} 
+
+markdownView.addEventListener('keyup', e => {
+    //console.log(e.target.value)
+    const currentContent = e.target.value;
+    renderToMarkdown(currentContent)
+})
